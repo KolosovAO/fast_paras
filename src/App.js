3116d7aba12fe4100e6688b18c3d68e0babb6c93
akolosov <jora364@gmail.com>
2022-03-13 02:50:42 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import { Paras } from "./Paras";
 
 function App() {
   const [collection_url, setCollectionUrl] = useState("");
+  const [delay, setDelay] = useState(2);
+  const [pages, setPages] = useState(1);
 
   return (
     <div>
+      <label>collection:</label>
       <input value={collection_url} onChange={e => setCollectionUrl(e.target.value)} />
-      {collection_url.endsWith(".near") && <Paras collection_url={collection_url} />}
+      <label>request delay, s:</label>
+      <input type="number" value={delay} onChange={e => setDelay(+e.target.value)} />
+      <label>pages(30):</label>
+      <input type="number" value={pages} onChange={e => setPages(+e.target.value)} />
+      {collection_url.endsWith(".near") && <Paras collection_url={collection_url} timeout={delay} pages={pages} />}
     </div>
   );
 }
--- src/Paras.jsx
+++ src/Paras.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 const getRanks = async (collection_url) => {
     const response = await fetch(`https://api.neararity.com/tokens?collection=${collection_url}&page=1&itemsPerPage=3500&keyword=&sortBy=rank`);
@@ -12,33 +11,53 @@ const getRanks = async (collection_url) => {
     }, {});
 }
 
-const loadData = async (collection_url) => {
-    const fetch_result = await fetch(`https://api-v2-mainnet.paras.id/token-series?collection_id=${collection_url}&exclude_total_burn=true&__limit=30&__sort=lowest_price::1&lookup_token=true`);
-    return await fetch_result.json();
+const loadData = async (collection_url, pages = 1, prev = []) => {
+    let url = `https://api-v2-mainnet.paras.id/token-series?collection_id=${collection_url}&exclude_total_burn=true&__limit=30&__sort=lowest_price::1&lookup_token=true`;
+    if (prev.length) {
+        const last = prev[prev.length - 1];
+        url += `&_id_next=${last._id}&lowest_price_next=${last.token.price.$numberDecimal}`;
+    }
+    const fetch_result = await fetch(url);
+    const json = await fetch_result.json();
+    const res = json.data.results;
+    if (pages === 1) {
+        return prev.concat(res);
+    }
+
+    return loadData(collection_url, pages - 1, prev.concat(res));
 }
 
-export const Paras = ({ collection_url }) => {
+export const Paras = ({ collection_url, timeout, pages }) => {
+    const timeout_ref = useRef(timeout);
+    const pages_ref = useRef(pages);
     const [ranks, setRanks] = useState({});
     const [items, setItems] = useState([]);
 
+    timeout_ref.current = timeout;
+    pages_ref.current = pages;
+
     useEffect(() => {
         getRanks(collection_url).then(setRanks);
-        loadData(collection_url).then((res) => setItems(res.data.results));
 
-        const interval = setInterval(() => {
-            loadData(collection_url).then((res) => setItems(res.data.results));
-        }, 3000);
-        return () => clearInterval(interval);
+        let id;
+        const reload = () => {
+            id = setTimeout(() => {
+                loadData(collection_url, pages_ref.current).then(setItems).then(reload);
+            }, timeout_ref.current * 1000);
+        };
+
+        loadData(collection_url, pages_ref.current).then(setItems).then(reload);
+
+        return () => clearTimeout(id);
     }, [collection_url]);
 
-
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: "column", flexWrap: "wrap", height: window.innerHeight - 100 }}>
             {items.map(({ token }) => {
                 return (
-                    <div key={token._id} style={{ display: "flex" }}>
-                        <div style={{ width: "200px" }}>rank: {ranks[token.token_id]}</div>
-                        <div style={{ width: "200px" }}>price: {(token.price.$numberDecimal / 10e23).toFixed(2)}</div>
+                    <div key={token._id} style={{ display: "flex", borderRight: "1px solid black" }}>
+                        <div style={{ width: 100, color: ranks[token.token_id] < 500 ? "red" : undefined }}>rank: {ranks[token.token_id]}</div>
+                        <div style={{ width: 100 }}>price: {(token.price.$numberDecimal / 10e23).toFixed(2)}</div>
                         <a rel="noreferrer noopener" target="_blank" href={`https://paras.id/token/${collection_url}::${token.token_id}/${token.token_id}`}>buy</a>
                     </div>
                 )
